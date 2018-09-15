7799419299b541e735d7da4dfad06880ad81b130
Unknown <ben.mozilla@gmail.com>
2018-09-14 23:29:56 -0400

    Update for Paul
    Movement Script added, images for mini game added to assets

---
+++ Stick_Guy/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    public void Move () {
+        transform.Translate(Vector2.right*moveSpeed*Time.deltaTime);
+    }
+
+}
