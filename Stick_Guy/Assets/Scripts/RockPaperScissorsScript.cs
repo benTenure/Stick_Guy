3bfd3e6c73e675623781f886f2a719a9ad9f901a
Robert Brosh <rbrosh1@umbc.edu>
2018-09-15 15:43:38 -0400

    Update to RockPaperScissor Game. Not Finished

--- Stick_Guy/Assets/Scripts/RockPaperScissorsScript.cs
+++ Stick_Guy/Assets/Scripts/RockPaperScissorsScript.cs
@@ -4,12 +4,48 @@ using UnityEngine;
 
 public class RockPaperScissorsScript : MonoBehaviour {
 
-    public enum RPSValue { Rock, Paper, Scissors};
-
+    //public enum RPSValue { Rock, Paper, Scissors};
+    public int RPSValue1;
+    public int RPSValue2;
+    public GameObject p1;
+    public GameObject p2;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //spawn rps options for pl1
+
+        //spawn rps options for pl2
+
+        RPSValue1 = Choose(p1);
+        RPSValue2 = Choose(p2);
+
+    }
+    
+    void Fight()
+    {
+        //If Player 1 wins
+        if ((RPSValue1 == 1 && RPSValue2 == 3) || (RPSValue1 == 2 && RPSValue2 == 1) || (RPSValue1 == 3 && RPSValue2 == 2))
+            p1.GetComponent<PlayerClass>()
+        
+        else if ((RPSValue2 == 1 && RPSValue1 == 3) || (RPSValue2 == 2 && RPSValue1 == 1) || (RPSValue2 == 3 && RPSValue1 == 2))
+
+        else
+
+    }
+
+    int Choose(GameObject)
+    {
+        int choice = 0;
+        while (choice == 0)
+        {
+            if (Input.GetKeyDown("r"))
+                choice = 1;
+            else if (Input.GetKeyDown("p"))
+                choice = 2;
+            else if (Input.GetKeyDown("s"))
+                choice = 3;
+        }
+        return choice;
+    }
 	
 	// Update is called once per frame
 	void Update () {
