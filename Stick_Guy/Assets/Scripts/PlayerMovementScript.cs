c23728d134d660d3ece64d346ef1ff4a19c6d5c7
Paul Koh <paulkoh94@gmail.com>
2018-09-15 00:19:25 -0400

    Player movement update
    Added movement script and playerclass script. Created Player prefab

--- Stick_Guy/Assets/Scripts/PlayerMovementScript.cs
+++ Stick_Guy/Assets/Scripts/PlayerMovementScript.cs
@@ -7,18 +7,41 @@ public class PlayerMovementScript : MonoBehaviour {
     [SerializeField]
     private float moveSpeed;
 
+    private Vector2 direction;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        GetInput();
         Move();
 	}
 
-    public void Move () {
-        transform.Translate(Vector2.right*moveSpeed*Time.deltaTime);
+    public void Move(){
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
     }
 
+    private void GetInput(){
+
+        direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.W)){
+            direction += Vector2.up;
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            direction += Vector2.down;
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            direction += Vector2.left;
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            direction += Vector2.right;
+        }
+    }
 }
