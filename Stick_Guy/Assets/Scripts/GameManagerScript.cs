66fad694af70ac6fbd9bb40122b9b4fee2c65d98
Unknown <ben.mozilla@gmail.com>
2018-09-14 21:45:49 -0400

    New Scripts
    PlayerClass and GameManager scripts started

---
+++ Stick_Guy/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    //Hardcode 2 players for now
+    public GameObject p1;
+    public GameObject p2;
+    private PlayerClass p1Script;
+    private PlayerClass p2Script;
+
+    //Check who wins a mini game (of 2 players, best of 5)
+    void whoIsWinning(PlayerClass p1, PlayerClass p2) {
+
+        if (p1.getWins() == 3) {
+            Win(p1);
+        }
+        else if (p2.getWins() == 3) {
+            Win(p2);
+        }
+    }
+
+    //Called when object is instantiated
+    private void Start() {
+        p1Script = p1.GetComponent<PlayerClass>();
+        p2Script = p2.GetComponent<PlayerClass>();
+    }
+
+    //Selected player wins the game
+    void Win(PlayerClass p) {
+
+    }
+
+    void startChallenge() {
+
+    }
+}
---
+++ Stick_Guy/Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass : MonoBehaviour {
+
+    int wins;
+
+    //Default Constructor
+    PlayerClass() {
+        wins = 0;
+    }
+
+    //If player wins a mini game
+    void winGame() {
+        wins++;
+    }
+
+    //If player loses a mini game
+    void loseGame() {
+        wins--;
+    }
+
+    //Getter for wins memeber (used by game manager)
+    public int getWins() {
+        return wins;
+    }
+
+}
