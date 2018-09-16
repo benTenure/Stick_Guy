647eb4177f337249047c5e2c9f97ab535f85728a
Unknown <ben.mozilla@gmail.com>
2018-09-16 15:01:04 -0400

    Auto stash before merge of "master" and "origin/master"

--- Stick_Guy/Assets/Scripts/RockPaperScissorsScript.cs
+++ Stick_Guy/Assets/Scripts/RockPaperScissorsScript.cs
@@ -9,35 +9,36 @@ public class RockPaperScissorsScript : MonoBehaviour {
     public int RPSValue2;
     public GameObject p1;
     public GameObject p2;
+
 	// Use this for initialization
-	void Start () {
-        //spawn rps options for pl1
-
-        //spawn rps options for pl2
-
-        RPSValue1 = Choose(p1);
-        RPSValue2 = Choose(p2);
-
+	void Start ()
+    {
+        //RPSValue1 = Choose(p1);
+        //RPSValue2 = Choose(p2);
     }
     
     void Fight()
     {
-        //If Player 1 wins
-        if ((RPSValue1 == 1 && RPSValue2 == 3) || (RPSValue1 == 2 && RPSValue2 == 1) || (RPSValue1 == 3 && RPSValue2 == 2))
-            p1.GetComponent<PlayerClass>();
-        else if ((RPSValue2 == 1 && RPSValue1 == 3) || (RPSValue2 == 2 && RPSValue1 == 1) || (RPSValue2 == 3 && RPSValue1 == 2))
+        bool winner = false;
+
+        while (winner == false)
         {
-
+            //If Player 1 wins
+            if ((RPSValue1 == 1 && RPSValue2 == 3) || (RPSValue1 == 2 && RPSValue2 == 1) || (RPSValue1 == 3 && RPSValue2 == 2))
+            {
+                p1.GetComponent<PlayerClass>().winGame();
+                winner = true;
+            }
+            else if ((RPSValue2 == 1 && RPSValue1 == 3) || (RPSValue2 == 2 && RPSValue1 == 1) || (RPSValue2 == 3 && RPSValue1 == 2))
+            {
+                p2.GetComponent<PlayerClass>().winGame();
+                winner = true;
+            }
         }
-        else{
-
-        }
-
+        
     }
 
-    //What is this supposed to do?
-    //It needed a name for the game object being referenced, so I just selected a placeholder name "p1"
-    int Choose(GameObject p1)
+   /* int Choose(GameObject player)
     {
         int choice = 0;
         while (choice == 0)
@@ -51,6 +52,7 @@ public class RockPaperScissorsScript : MonoBehaviour {
         }
         return choice;
     }
+    */
 	
 	// Update is called once per frame
 	void Update () {
