829a45016dda934a5562765f6d471ddf3f4751fc
GamerXNin46 <toonsisme@gmail.com>
2018-09-16 15:55:12 -0400

    updated pong movements

--- Stick_Guy/Assets/Movementp2.cs
+++ Stick_Guy/Assets/Movementp2.cs
@@ -2,23 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementP1 : MonoBehaviour {
+public class Movementp2 : MonoBehaviour
+{
 
     [SerializeField]
     private float moveSpeed;
     private Vector2 direction;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         GetInput();
         Move();
     }
 
+    //it moves in the direction
     public void Move()
     {
         transform.Translate(direction * moveSpeed * Time.deltaTime);
@@ -29,12 +33,12 @@ public class MovementP1 : MonoBehaviour {
     {
 
         direction = Vector2.zero;
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             direction += Vector2.up;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             direction += Vector2.down;
         }
