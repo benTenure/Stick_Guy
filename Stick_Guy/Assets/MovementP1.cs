33b6e68f6999138016a4ae5dfbd93a6eadfc4539
GamerXNin46 <toonsisme@gmail.com>
2018-09-16 15:46:28 -0400

    adding pong animation

---
+++ Stick_Guy/Assets/MovementP1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementP1 : MonoBehaviour {
+    private float moveSpeed;
+    private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetInput();
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+
+    //it will move up and down
+    private void GetInput()
+    {
+
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+
+    }
+}
