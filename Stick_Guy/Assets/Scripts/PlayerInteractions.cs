8e49499b6649b54188dc63cd0a52796240f1590d
RobertAvolio <ravolio1@umbc.edu>
2018-10-19 14:42:13 -0400

    Arcade Cabinet

---
+++ Stick_Guy/Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour {
+
+    public GameObject prompt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+       
+        if (col.gameObject.name == "Arcade Trigger")
+        {
+            Debug.Log("We found a " + col.gameObject.name);
+            prompt.gameObject.SetActive(true);
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        Debug.Log("Come back again sooN!");
+
+        if (col.gameObject.name == "Arcade Trigger")
+        {
+            prompt.gameObject.SetActive(false);
+        }
+    }
+}
