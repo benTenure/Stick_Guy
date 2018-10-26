b10d9c7979436b0a9e7766b4f2cf8a5204894685
RobertAvolio <ravolio1@umbc.edu>
2018-10-26 13:07:23 -0400

    The start of Breakout
    Set the scene, made objects, made some scripts, all that works so far is the paddle moving and colliding.

---
+++ Stick_Guy/Assets/Scripts/BallInteraction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Brick"))
+        {
+            // check whether the box was hit on it's side or bottom 
+
+            
+            // Update ball's velocity
+            
+            
+        }
+    }
+
+    private void changeTrajectoryHorizontal()
+    {
+        // x movement * -1
+    }
+
+    private void changeTrajectoryVertical()
+    {
+        // y movement * -1
+    }
+}
---
+++ Stick_Guy/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+    public float moveSpeed = 10f;
+    private Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        /*
+        float movement = Input.GetAxis("Horizontal");
+        Vector2 movementVector = new Vector2(movement, 0);
+        rb.AddForce(movementVector * moveSpeed); 
+        */
+        Vector2 movementVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementVector.x = (transform.right * Time.deltaTime * -moveSpeed).x;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementVector.x = (transform.right * Time.deltaTime * moveSpeed).x;
+        }
+            
+        movementVector = movementVector + (Vector2)(transform.position);
+
+        rb.MovePosition(movementVector);
+    }
+
+}
