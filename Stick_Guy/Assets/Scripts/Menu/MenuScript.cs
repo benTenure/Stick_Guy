f360faa74a3b704e942542b6fa797aba95908af6
Paul <pkoh94@gmail.com>
2019-04-19 16:46:04 -0400

    Menu

---
+++ Stick_Guy/Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
