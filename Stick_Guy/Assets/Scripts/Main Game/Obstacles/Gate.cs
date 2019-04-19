90748c490fd1e0c22c801070196ce61ee62f6f52
Robert Avolio <ravolio1@umbc.edu>
2019-04-19 16:36:23 -0400

    Tons of changes
    Art added to obstacles, prefabs made for them. Started redesigning the enemy spawners.

---
+++ Stick_Guy/Assets/Scripts/Main Game/Obstacles/Gate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public EnemySpawnerScript e1;
+    public EnemySpawnerScript e2;
+    public EnemySpawnerScript e3;
+    public EnemySpawnerScript e4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+
+            e1.gameObject.SetActive(true);
+            e2.gameObject.SetActive(true);
+            e3.gameObject.SetActive(true);
+            e4.gameObject.SetActive(true);
+        }
+    }
+}
